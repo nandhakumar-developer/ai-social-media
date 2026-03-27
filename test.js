a3a3e1878bf8ea85f91e05e3a77a79f947218ee9
V.Nandhakumar <162693004+Mr-tamilan-yt@users.noreply.github.com>
2026-03-27 13:18:31 +0530

    noce

--- test.js
+++ test.js
@@ -1,5 +1,7 @@
 import fs from 'fs';
-const key = "AIzaSyCtngc9m3O_KJ2IUfy75XJ4e7NbN_98fM4";
+
+const key = process.env.GEMINI_API_KEY;
+
 fetch(`https://generativelanguage.googleapis.com/v1beta/models?key=${key}`)
   .then(r => r.json())
   .then(data => {
